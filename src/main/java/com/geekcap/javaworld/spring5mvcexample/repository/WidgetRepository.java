fca8dd5ff13d4233ef8123e583ad5659677f5bae
Vasily Shcherbinin <vs00162@surrey.ac.uk>
2018-08-17 23:00:01 +0200

    Initial commit

---
+++ src/main/java/com/geekcap/javaworld/spring5mvcexample/repository/WidgetRepository.java
@@ -0,0 +1,8 @@
+package com.geekcap.javaworld.spring5mvcexample.repository;
+
+import com.geekcap.javaworld.spring5mvcexample.model.Widget;
+import org.springframework.data.repository.CrudRepository;
+
+public interface WidgetRepository extends CrudRepository<Widget, Long> {
+
+}
